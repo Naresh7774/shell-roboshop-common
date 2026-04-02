93d95372f5b66ef6a553aa3174abbf24ec61fd9b
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-02 23:09:26 +0530

    project

--- catalogue.sh
+++ catalogue.sh
@@ -13,3 +13,11 @@ VALIDATE $? "Copy mongo repo"
 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Install MongoDB client"
+
+INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Load $app_name products"
+else
+    echo -e "$app_name products already loaded ... $Y SKIPPING $N"
+fi
