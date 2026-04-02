f0c33ee263623d77f2bd4a27dfed6178c60dab00
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-02 23:09:18 +0530

    project

--- catalogue.sh
+++ catalogue.sh
@@ -7,3 +7,9 @@ check_root
 app_setup
 nodejs_setup
 systemd_setup
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copy mongo repo"
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Install MongoDB client"
