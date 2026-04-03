665af5ac6c91efb9d934b2de8c596dbd1cb7a4e6
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-03 23:00:19 +0530

    project

--- frontend.sh
+++ frontend.sh
@@ -48,3 +48,8 @@ VALIDATE $? "Downloading frontend"
 rm -rf /etc/nginx/nginx.conf
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
 VALIDATE $? "Copying nginx.conf"
+
+
+
+systemctl restart nginx 
+VALIDATE $? "Restarting Nginx"
