9a7a4bbb0d55678ed5f7993dc423e1b1f087acca
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-03 22:59:39 +0530

    project

--- frontend.sh
+++ frontend.sh
@@ -29,3 +29,8 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
     fi
 }
 
+dnf module disable nginx -y &>>$LOG_FILE
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "Installing Nginx"
+
