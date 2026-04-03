0b42a77eb381ca823cb259bbbfd73371a2b2343f
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-03 22:58:54 +0530

    project

--- frontend.sh
+++ frontend.sh
@@ -5,3 +5,9 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.daws86s.fun
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
